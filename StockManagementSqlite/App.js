819650a3ecd559b7f9a01df172f851654aa166bb
ct mac <cth.huynh@gmail.com>
2023-06-25 22:47:13 +0200

    update screens

--- StockManagementSqlite/App.js
+++ StockManagementSqlite/App.js
@@ -1,22 +1,42 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomePage from "./screens/HomePage";
+import LoginScreen from "./screens/LoginSreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import DashBoard from "./screens/DashBoard";
+import ForgortPassWordScreen from "./screens/ForgorPassWordScreen";
+const Stack = createNativeStackNavigator();
+import { Provider } from 'react-redux';
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import * as Sqlite from 'expo-sqlite';
 import { useState, UseEffect } from 'react';
-export default function App() {
+
+
+
+const App = () => {
   return (
-
-    <View style={styles.container}>
-      <Text>Bienvenue {}</Text>
-      <Text>Cliquez pour vous connectez</Text>
-      <Button onPress={()=> navigate('LoginScreen')}
-      title='Connexion'></Button>
-
-      <StatusBar style="auto" />
-    </View>
-    
-
+      <Provider store={Store}>
+          <NavigationContainer>
+              <Stack.Navigator>
+                  <Stack.Screen
+                      name="Homescreen"
+                      component={HomePage}
+                  />
+                  <Stack.Screen name="Loginscreen" component={LoginScreen} />
+                  <Stack.Screen name="Registerscreen" component={RegisterScreen} />
+                  <Stack.Screen name="ForgotPasswordscreen" component={ForgortPassWordScreen} />
+                  <Stack.Screen name="Dashboard" component={DashBoard} />
+              </Stack.Navigator>
+          </NavigationContainer>
+      </Provider>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ StockManagementSqlite/screens/LoginSreen.js
@@ -0,0 +1,12 @@
+
+
+<View style={styles.container}>
+<Text>Bienvenue {}</Text>
+<Text>Cliquez pour vous connectez</Text>
+
+<Button onPress={()=> navigate('LoginScreen')}
+title='Connexion'></Button>
+
+<StatusBar style="auto" />
+</View>
+

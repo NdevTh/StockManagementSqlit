19f7e728f3ee37213b8327ac8f0684ed3edfda0f
ct mac <cth.huynh@gmail.com>
2023-07-05 16:44:34 +0200

    update PhotoModif and PhotoListScreen ok

--- StockManagementSqlite/screens/PhotoListScreen copy.js
+++ StockManagementSqlite/screens/PhotoListScreen copy.js
@@ -1,94 +1,11 @@
-import React from 'react';
-import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { retrievePhotosFromDB, deletePhotoFromDB } from './Database.js';
-
-export default class PhotoListScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            photos: []
-        };
-    }
-
-    componentDidMount() {
-        this.fetchPhotos();
-    }
-
-    fetchPhotos = () => {
-        retrievePhotosFromDB((photos) => {
-            this.setState({ photos: photos });
-        });
-    };
-
-    deletePhoto = (id) => {
-        deletePhotoFromDB(id, (success) => {
-            if (success) {
-                this.fetchPhotos();
-            } else {
-                console.log('Erreur de suppression de la photo');
-            }
-        });
-    };
-
-    renderItem = ({ item }) => {
-        return (
-            <View style={styles.container}>
-                <Text>{item.designation}</Text>
-                <Text>{item.quantity}</Text>
-                <Image source={{ uri: item.image }} style={styles.image} />
-                <TouchableOpacity
-                    style={styles.deleteButton}
-                    onPress={() => this.deletePhoto(item.id)}
-                >
-                    <Text style={styles.deleteButtonText}>Supprimer</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    };
-
-    render() {
-        return (
-            <View>
-                <FlatList
-                    data={this.state.photos}
-                    keyExtractor={item => item.id.toString()}
-                    renderItem={this.renderItem}
-                />
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        paddingBottom: 10,
-    },
-    image: {
-        width: 100,
-        height: 100,
-    },
-    deleteButton: {
-        backgroundColor: 'red',
-        padding: 10,
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    deleteButtonText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
-});
-
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity, TextInput, Button } from 'react-native';
 import { retrievePhotosFromDB, deletePhotoFromDB } from './Database.js';
 
 const PhotoListScreen = ({ navigation }) => {
   const [photos, setPhotos] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [filteredPhotos, setFilteredPhotos] = useState([]);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', fetchPhotos);
@@ -97,9 +14,15 @@ const PhotoListScreen = ({ navigation }) => {
     return unsubscribe;
   }, [navigation]);
 
+  useEffect(() => {
+    // Effectuer la recherche à chaque fois que le texte de recherche change
+    performSearch();
+  }, [searchText]);
+
   const fetchPhotos = () => {
     retrievePhotosFromDB((photos) => {
       setPhotos(photos);
+      setFilteredPhotos(photos);
     });
   };
 
@@ -111,6 +34,15 @@ const PhotoListScreen = ({ navigation }) => {
         console.log('Erreur de suppression de la photo');
       }
     });
+  };
+
+  const performSearch = () => {
+    const filtered = photos.filter((photo) => {
+      const designation = photo.designation.toLowerCase();
+      const searchTextLower = searchText.toLowerCase();
+      return designation.includes(searchTextLower);
+    });
+    setFilteredPhotos(filtered);
   };
 
   const renderItem = ({ item }) => {
@@ -135,16 +67,23 @@ const PhotoListScreen = ({ navigation }) => {
 
   return (
     <View>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Rechercher..."
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+        <Button title="OK" onPress={performSearch} />
+      </View>
       <FlatList
-        data={photos}
+        data={filteredPhotos}
         keyExtractor={(item) => item.id.toString()}
         renderItem={renderItem}
       />
     </View>
   );
 };
-
-// new code valide 
 
 const styles = StyleSheet.create({
   container: {
@@ -173,6 +112,18 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center',
     marginTop: 10,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 10,
+  },
+  searchInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 10,
+    paddingHorizontal: 10,
   },
 });
 

3063d7effb73e74a2bd994a5df0c81f67fa378d8
ct mac <cth.huynh@gmail.com>
2024-05-11 08:43:56 +0200

    creat style file

--- StockManagementSqlite/screens/stock.js
+++
@@ -1,6 +0,0 @@
-import { isEqualIcon } from "react-native-paper/lib/typescript/src/components/Icon"
-
-je stock isEqualIcon
-Je t'arrive pas a push sur git !!!!!
-comment je dois faire pour réussir ? 
-comment je teste mon code sur pc , 
---
+++ StockManagementSqlite/screens/style.js
@@ -0,0 +1,49 @@
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        margin: 20,
+    },
+    button: {
+        flex: 0.1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    buttonSnap: {
+        marginTop: 20,
+        flex: 0.3,
+        alignSelf: 'flex-start',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        color: 'white',
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginRight: 10,
+    },
+    image: {
+        flex: 1,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+    }
+});
